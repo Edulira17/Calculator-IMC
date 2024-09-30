3b4d372d2908eefd1bb9a45c7ce71739c2d00f7e
Eduardo Lira <eduardoliracunha17@gmail.com>
2024-09-30 14:25:18 -0400

    first commit: projeto 100%

---
+++ README.md
@@ -0,0 +1,3 @@
+# CALCULADORA DE IMC COM REACT
+
+Objetivo do projeto é colocar em prática o que aprendi nos cursos de React que ando fazendo!
---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+import { data } from './data/data'
+
+import ImcCalc from './components/CalculadoraIMC/ImcCalc'
+import ImcTable from './components/ImcTable/ImcTable';
+import './App.css'
+
+function App() {
+  // Função de cálculo do IMC:
+  const calcImc = (e, height, weight) => {
+    e.preventDefault();
+
+    if (!weight || !height) return;
+
+    // Converte o texto para número e altera a "," para o ".";
+    const weightFloat = +weight.replace(",", ".");
+    const heightFloat = +height.replace(",", ".");
+
+    // Fórmula do calculo do IMC:
+    const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1);
+
+    setImc(imcResult)
+
+    data.forEach((item) => {
+      if (imcResult >= item.min && imcResult <= item.max) {
+        setInfo(item.info)
+        setInfoClass(item.infoClass)
+      }
+    });
+
+    if(!info) return;
+  }
+
+
+  const resetCalc = (e) => {
+    e.preventDefault();
+    setImc("")
+    setInfo("")
+    setInfoClass("")
+  }
+
+  const [imc, setImc] = useState('');
+  const [info, setInfo] = useState('');
+  const [infoClass, setInfoClass] = useState('');
+
+
+  return (
+    <div className='container'>
+      {!imc ? (
+        <ImcCalc calcImc={calcImc} />
+      ) : (
+        <ImcTable data={data} imc={imc} info={info} infoClass={infoClass} resetCalc={resetCalc}/>
+      )}
+    </div>
+  )
+}
+
+export default App
